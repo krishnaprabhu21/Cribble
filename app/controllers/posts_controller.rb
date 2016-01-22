7c4d3df0a4546a2987e866bf86b2c0c66676bbfc
Krishna Prabhu <krishnaprabhu21@gmail.com>
2016-01-22 15:26:14 +0530

    Image upoading added using paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
 
 	def update
-		if @post.upate(post_params)
+		if @post.update(post_params)
 			redirect_to @post
 		else
 			render 'edit'
@@ -60,7 +60,7 @@ class PostsController < ApplicationController
 		end
 
 		def post_params
-			params.require(:post).permit(:title, :link, :description)
+			params.require(:post).permit(:title, :link, :description, :image)
 		end
 
 
