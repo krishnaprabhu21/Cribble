ce0489b336ac1e50c207a4e6f5653ece78c42594
Krishna Prabhu <krishnaprabhu21@gmail.com>
2016-01-22 14:48:48 +0530

    CRUD operations added for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,66 @@
 class PostsController < ApplicationController
 
+	before_action :find_post, only: [:show, :edit, :update, :destroy] 
+
 	def index
+		@post = Post.all.order("created_at DESC")
 	end
+
+
+	def show
+		
+	end
+
+
+	def new
+		@post = Post.new
+	end
+
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+
+	def update
+		if @post.upate(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end			
+	end
+
+
+	def destroy
+		@post = Post.find(params[:id])
+
+		if @post.present?
+			@post.destroy
+		end
+
+		redirect_to root_path
+	end
+
+
+	private
+
+		def find_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :link, :description)
+		end
+
 
 end
